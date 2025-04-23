887dcbb751a4e85f5bf0b3b6b03df1821b7ce2c8
“Thirumaran <Thirumaran.dk@gmail.com>
2025-04-23 00:37:29 -0700

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  basePath: '/ThiruDigitalResume',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig 
